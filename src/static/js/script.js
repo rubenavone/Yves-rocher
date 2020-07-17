6ea0002c41ed3a2c20cc1ec2e81b49d4002b4041
Ruben <navone_ruben@msn.com>
2020-07-17 14:37:56 +0200

    division du sass en plusieur module, creation du menu responsive (à terminer), changement sur les placement en générale

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,23 @@
-console.log(
-  "Il n'y a rien a voir ici une journée de plus m'aurais permis de faire du javascript"
-);
+"use strict";
+
+let openMenu = document.querySelector(".menu-open-js");
+let closeMenu = document.querySelector(".menu-close-js");
+let navOne = document.querySelector(".nav-one");
+let navTwo = document.querySelector(".nav-two");
+
+console.log(openMenu, closeMenu);
+
+const handleOpenMenu = (e) => {
+  console.log("dans open");
+  navOne.classList.remove("active");
+  navTwo.classList.add("active");
+};
+
+const handleCloseMenu = (e) => {
+  console.log("dans close");
+  navOne.classList.add("active");
+  navTwo.classList.remove("active");
+};
+
+openMenu.addEventListener("click", handleOpenMenu, false);
+closeMenu.addEventListener("click", handleCloseMenu, false);
